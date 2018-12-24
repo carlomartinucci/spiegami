322cc6a094e5e0fd3bf68fe2603c5e9ca925356c
Daniel <pettenon.daniel@gmail.com>
2018-12-24 13:57:51 +0100

    Style up more the blocks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,10 @@
 //= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
+
+$(document).on('click', '.inner-block.with-referenced-blocks .card', function(e) {
+  target = $(e.target);
+  $.ajax({
+    url: '/blocks/' + target.closest('[data-block-id]').attr('data-block-id') + '/referenced_blocks'
+  })
+});
