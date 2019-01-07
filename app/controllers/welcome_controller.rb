afe3bc8e9452676d71cb960993499041a5049c41
Carlo Martinucci <carlo.martinucci@gmail.com>
2019-01-08 00:00:59 +0100

    add prototype

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
-
   def index
     @spiegami_block = Block.find_by(title: 'Spiegami è utile per formarsi un\'opinione')
   end
 
+  def prototype
+    render layout: 'empty'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :blocks do
-    member do 
+    member do
       get :referenced_blocks
     end
   end
 
   root 'welcome#index'
+  get :prototype, to: 'welcome#prototype', as: :prototype
 end
